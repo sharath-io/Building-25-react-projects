ec050f560fbc9bc56b13904d03084e32cad6a185
Sharath <sharath.esr@gmail.com>
2024-09-23 20:06:58 +0530

    feat: add multiple selection for accordion

--- src/components/Accordian.jsx
+++ src/components/Accordian.jsx
@@ -1,29 +1,52 @@
-import { useState } from "react";
-import {data} from "./data";
+import { useEffect, useState } from "react";
+import { data } from "./data";
 
-export function Accordion(){
-    const [displayItem, setDisplayItem] = useState(null)
+export function Accordion() {
+  const [displayItem, setDisplayItem] = useState(null);
+  const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+  const [multiple, setMuliple] = useState([]);
 
-    const handleSingleSelection =(id) =>{
-       displayItem===id ? setDisplayItem(null) : setDisplayItem(id)
-    }
+  const handleSingleSelection = (id) => {
+    displayItem === id ? setDisplayItem(null) : setDisplayItem(id);
+  };
 
-return (
-<>
+  const handleMultipleSelection = (id) => {
+    multiple.includes(id)
+      ? setMuliple([...multiple.filter((item) => item !== id)])
+      : setMuliple([...multiple, id]);
+  };
 
-    <ul className="flex flex-col justify-center items-center m-auto w-3/4">
-    <button>Single</button>
-        {
-        data.map(({id,question,answer}) =>
-        <li key={id} className="bg-yellow-700 my-2 w-2/4 p-2">
-            <div className="flex justify-between">
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center m-auto w-3/4">
+        <h1> Accordion component</h1>
+        <button
+          className=" bg-green-900 p-3 my-3 text-white w-content rounded-md"
+          onClick={() => setEnableMultipleSelection(!enableMultipleSelection)}
+        >
+          {enableMultipleSelection ? "Multiple Selection" : "Single Selection"}
+        </button>
+
+        <ul className="flex flex-col justify-center items-center m-auto w-3/4">
+          {data.map(({ id, question, answer }) => (
+            <li key={id} className="bg-yellow-700 my-2 w-2/4 p-2 ">
+              <div className="flex justify-between">
                 <h1>{question}</h1>
-                <button onClick={()=> handleSingleSelection(id)}>+</button>
-            </div>
-        {displayItem===id && <p>{answer}</p>}
-        </li>)
-        }
-    </ul>
-</>
-)
+                <button
+                  onClick={() =>
+                    enableMultipleSelection
+                      ? handleMultipleSelection(id)
+                      : handleSingleSelection(id)
+                  }
+                >
+                  +
+                </button>
+              </div>
+              {(multiple.includes(id) || displayItem === id) && <p>{answer}</p>}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
